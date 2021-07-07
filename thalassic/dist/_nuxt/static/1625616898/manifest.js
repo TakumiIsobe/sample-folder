55a4482802ec70de02a5235e2b25be30f9b1671d
Takumi Isobe <62628708+TakumiIsobe@users.noreply.github.com>
2021-07-07 09:16:20 +0900

    Add files via upload

---
+++ thalassic/dist/_nuxt/static/1625616898/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fabout","\u002Fgiving","\u002F","\u002Fcontact","\u002Fequity","\u002Fpartnership","\u002Freal-estate"]})
--- thalassic/dist/anime.js
+++ thalassic/dist/anime.js
@@ -1,8 +1,6 @@
-if(location.href=='http://192.168.1.52:3000/'){
+//if(location.href=='http://192.168.1.52:3000/'){
 gsap.config({nullTargetWarn: false});
-
 const tl = gsap.timeline();
-
 
 tl.set('main', {opacity: 0}) 
 tl.set('.op-wrapper', {opacity: 0}) 
@@ -20,22 +18,17 @@ tl.from('.top h1 .box1 span', 2, {opacity: 0, y: 30, ease: Expo.easeInOut, stagg
 tl.from('.top h1 .box2 span', 2, {opacity: 0, y: 30, ease: Expo.easeInOut, stagger: 0.025}, "op+=3.5") 
 tl.from('.top h1 .box3 span', 2, {opacity: 0, y: 30, ease: Expo.easeInOut, stagger: 0.025}, "op+=3.5") 
 tl.from('.top p', 2, {opacity: 0, yPercent: 10, ease: Expo.easeOut}, "op+=5") 
-
+//}
 
 
 /*
-tl.set('main', {css: {autoAlpha: 0}}) 
-tl.to('.op-wrapper', {css: {opacity: 1, duration: 1}, ease: Expo.easeInOut}, "op") 
-//tl.to('.op-wrapper', {css: {opacity: 1, duration: 1}, ease: Expo.easeInOut}, "op+=.5") 
-tl.to('.op-wrapper svg path', 2.5, {css: {opacity: 1}, ease: Expo.easeInOut, stagger: 0.1}, "op+=1.5")
-*/
-}
-
 if(location.href=='http://localhost:3000/'){
     let preventer = document.getElementsByClassName('prevent');
     window.addEventListener('load', function() {
         preventer[0].classList.toggle('none');
     })
 };
+
+*/
 
 

368d042c61f325f2a0f8dded200d403757665e17
Takumi Isobe <62628708+TakumiIsobe@users.noreply.github.com>
2021-08-09 14:20:26 +0900

    Add files via upload

---
+++ thalassic/dist/js/header.js
@@ -0,0 +1,34 @@
+window.onload = function() {
+    const body = document.body;
+    const hamburger = document.getElementById('js-hamburger');
+    const blackBg = document.getElementById('js-black-bg');
+    hamburger.addEventListener('click', function() {
+      body.classList.toggle('nav-open');
+    }, false);
+    blackBg.addEventListener('click', function() {
+      body.classList.remove('nav-open');
+    }, false);
+};
+
+
+//Change bcColor after scroll
+window.addEventListener( "scroll", function() {
+  //var header = document.getElementsByTagName('header');    
+  const header = document.querySelector("header");
+  const rect = header.getBoundingClientRect();
+  const y = rect.top + window.pageYOffset; 
+  if (y > 0) {    
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+
+
+let body = document.body;
+const headerLinks = document.getElementsByClassName('headerLink');
+for ( let i = 0; i < 10; i++ ) {
+  headerLinks[i].addEventListener('click', function () {
+    body.classList.remove('nav-open');
+  })
+};
